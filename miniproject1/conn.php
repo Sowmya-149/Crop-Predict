0e4b7cde71ca0f73541b363009e37c5ad954f490
Sowmya-149 <72482451+Sowmya-149@users.noreply.github.com>
2023-03-28 12:29:01 +0530

    Add files via upload

---
+++ miniproject1/conn.php
@@ -0,0 +1,26 @@
+<?php 
+
+	$server = "localhost:3307";
+	$user = "root";
+	$password = "";
+	$db = "agriculture";
+	
+	$con = mysqli_connect($server, $user, $password, $db);
+
+	if($con){
+		/*?>
+		<script>
+			alert("Connection Successful");
+	    </script>
+	    <?php*/
+	}
+
+	else{
+		?>
+		<script>
+			alert("No Connection");
+	    </script>
+	    <?php
+	}
+
+?>
